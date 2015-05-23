fd1c2ef6392da5e2209cc185aa2a9682cb42b79b
Artur Araújo <arturaraujo.b@gmail.com>
2015-05-23 19:07:51 -0300

    Created packages to organize files

---
+++ src/br/edu/ifpb/tsi/pdm/pdmproject/broadcast/AlarmReceiver.java
@@ -0,0 +1,18 @@
+package br.edu.ifpb.tsi.pdm.pdmproject.broadcast;
+import br.edu.ifpb.tsi.pdm.pdmproject.service.AlarmService;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+	Context context;
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Intent service = new Intent(context, AlarmService.class);
+		service.putExtras(intent.getExtras());
+		context.startService(service);
+	}
+
+}
---
+++ src/br/edu/ifpb/tsi/pdm/pdmproject/service/AlarmService.java
@@ -0,0 +1,41 @@
+package br.edu.ifpb.tsi.pdm.pdmproject.service;
+
+import br.edu.ifpb.tsi.pdm.pdmproject.R;
+import br.edu.ifpb.tsi.pdm.pdmproject.R.drawable;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+
+public class AlarmService extends Service{
+
+	private NotificationManager mManager;
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		this.getApplicationContext();
+		mManager = (NotificationManager) this.getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+
+		NotificationCompat.Builder notification = new NotificationCompat.Builder(this);
+		notification.setTicker("Você tem tarefas a fazer!");
+		notification.setContentText(intent.getAction());
+
+		notification.setContentTitle(intent.getStringExtra("ATIVIDADE") + " de " + intent.getStringExtra("DISCIPLINA"));
+		notification.setContentInfo("Em " + intent.getStringExtra("DATA_HORA"));
+		notification.setSmallIcon(R.drawable.ic_launcher);
+		notification.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
+		notification.setAutoCancel(true);
+
+		mManager.notify(0, notification.build());
+		return super.onStartCommand(intent, flags, startId);
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+
+}
